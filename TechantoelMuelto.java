2350d0b3a37cefbc324c67113f99d105bbb62127
vichoRHLM <vicente.bravo.i@mail.pucv.cl>
2024-09-01 22:10:35 -0400

    Pr en la casa

---
+++ TechantoelMuelto.java
@@ -0,0 +1,14 @@
+package gestionavancecurricular;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Vicente
+ */
+public class TechantoelMuelto {
+    private Boolean telochanto = true;
+}
